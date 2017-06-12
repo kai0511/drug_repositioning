280d03c699c7bcfc655462e70a7ddbe411314a63
Kai <zhaokai09@gmail.com>
2017-06-12 12:11:54 +0800

    Update rank_coefs.py

--- rank_coefs.py
+++ rank_coefs.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import os
 
-def rankDataFrame(fileName, colName, sort_idx, toFile):
+def rankDataFrame(fileName, colName, sortIdx, toFile):
+    ''' fileName: the location of the file
+    colName: specifies the names of dataframe read from the file
+    sortIdx: sort the dataframe according to which element of colName
+    toFile: the file written to
+    '''
     coefs = pd.read_csv(fileName, header=None)
     coef_df = coefs.loc[:,1:]
     coef_df.columns = colName
-    rankedCoefs = coef_df.sort_values(by = colName[sort_idx], ascending = False)
+    rankedCoefs = coef_df.sort_values(by = colName[sortIdx], ascending = False)
     rankedCoefs.to_csv(toFile)
 
 
