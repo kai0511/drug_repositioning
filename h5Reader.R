7dff4922761ffc832f6dd1067c94a41c1d2c8110
Kai <zhaokai09@gmail.com>
2017-06-21 13:08:11 +0800

    Update h5Reader.R

--- h5Reader.R
+++ h5Reader.R
@@ -1,17 +1,21 @@
-loadhdf5data <- function(h5File, dataFrameName) {
-    # the function from the link: https://www.r-bloggers.com/load-a-pythonpandas-data-frame-from-an-hdf5-file-into-r/
-    require(h5) # available on CRAN
+require(h5)
 
-    f <- h5file(h5File)
-    nblocks <- h5attr(f[dataFrameName], "nblocks")
+getdfFromH5 <- function(i, object, f){
+    # get data frame from the i-th block of h5 object
+    d <- as.data.frame(f[paste0(object, "/block", i, "_values")][])
+    colnames(d) <- f[paste0(object, "/block", i, "_items")][]
+    return(d)
+}
+
+loadhdf5data <- function(loc, objectName) {
+    # obtain data frame named object from h5 object
+    f <- h5file(loc)
+    nblocks <- h5attr(f[objectName], "nblocks")
 
     df <- do.call(cbind, 
-                    lapply(seq_len(nblocks)-1, 
-                           function(i){
-                                data <- as.data.frame(f[paste0(dataFrameName, "/block", i, "_values")][])
-                                colnames(data) <- f[paste0(dataFrameName, "/block", i, "_items")][]
-                           }))
+                  lapply(seq(nblocks)-1, getdfFromH5, object = objectName, f = f))
     h5close(f)
-    return df
-    
+    return(df)
 }
+
+# pheno <- loadhdf5data(loc, objectName)
