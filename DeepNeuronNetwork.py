9f25f0ce0005337c05e4f5d8d6542264e55e382f
Kai <zhaokai09@gmail.com>
2017-06-29 18:15:53 +0800

    Update DeepNeuronNetwork.py

--- DeepNeuronNetwork.py
+++ DeepNeuronNetwork.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import os
+import re
 import warnings
 import hyperas
 import numpy as np
@@ -22,7 +23,7 @@ warnings.filterwarnings("ignore")
 
 class DeepNeuronNetwork(object):
 
-    def __init__(self, data, fileName, numFold = 3, standardize=False):
+    def __init__(self, X, y, fileName, maxEvals = 50, numFold = 3, standardize=False):
         ''' X input data, type: numpy.ndarray 
             y expected output, type: numpy.ndarray 
             params python dictionary used to set up neuron network
@@ -31,13 +32,12 @@ class DeepNeuronNetwork(object):
         # .values would transfer dataframe to an array; 
         # .iloc is a function to index row/columns in dataframe
         if standardize:
-            self.inputData = StandardScaler().fit_transform(data.iloc[:, 2:].values)  
+            self.inputData = StandardScaler().fit_transform(X)  
         else:
-            self.inputData = data.iloc[:, 2:].values
+            self.inputData = X
             
-        self.outputData = data.iloc[:, 1]
-        self.drugName = data.iloc[:, 0]
-        self.numFeature = self.outputData.shape[1]
+        self.outputData = y
+        self.numFeature = self.inputData.shape[1]
         self.train_X, self.train_y = None, None
         self.test_X, self.test_y = None, None
         self.model = None
@@ -45,7 +45,7 @@ class DeepNeuronNetwork(object):
         self.params = {}
         self.minLosses = []
         self.models = []
-        
+        self.maxEvals = maxEvals
         self.fileName = fileName  #'keras_deep_learning_prediction.out'
 
     def setParams(self):
@@ -54,7 +54,7 @@ class DeepNeuronNetwork(object):
         If you choose too few, eg 50, then all validation sample in the minibatch may have the same label 
         (validation loss will be the the same if all outcome variable is 0) 
         '''
-        parms = {'choice': hp.choice('num_layers',
+        params = {'choice': hp.choice('num_layers',
                                      [{'layers':'two', },
                                       {'layers':'three', 
                                        'units3': hp.uniform('units3', 64,1024), 
@@ -67,58 +67,59 @@ class DeepNeuronNetwork(object):
                  'dropout2': hp.uniform('dropout2',  .25,.75),
                  
                  # 'batch_size' : hp.uniform('batch_size', 256,1024),
-                 'batch_size' : self.train_y[0], # key change for unbalanced dataset 
+                 'batch_size' : self.train_y.shape[0], # key change for unbalanced dataset 
                  'l1_1st_layer': hp.uniform('l1_1st_layer',1e-5,1e-2), #switch order of the two parameters
                  'l2_1st_layer': hp.uniform('l2_1st_layer',1e-5,1e-2),
                  'nb_epochs' : 100, 
                  'optimizer': hp.choice('optimizer',['adadelta','adam','rmsprop']),
                  'activation':  hp.choice('activation',['relu','softplus','tanh'])}
                  
-        self.parms = parms
+        self.params = params
         
-    def constructNeuronNetwork(self):
+    def constructNeuronNetwork(self, params):
         ''' construct neuron network using Keras Sequential
         '''
         model = Sequential()
         model.add(Dense(input_dim = self.numFeature, 
-                        output_dim = self.params['units1'], 
-                        W_regularizer = l1l2(l1=self.params['l1_1st_layer'],l2=self.params['l2_1st_layer'])))
+                        output_dim = params['units1'], 
+                        W_regularizer = l1l2(l1=params['l1_1st_layer'],l2=params['l2_1st_layer'])))
                         
         # model.add(BatchNormalization())
         model.add(Activation('sigmoid'))
-        model.add(Dropout(self.params['dropout1']))
+        model.add(Dropout(params['dropout1']))
         
-        model.add(Dense(output_dim=self.params['units2'], 
-                        W_regularizer=l1l2(l1=self.params['l1_1st_layer'],l2=self.params['l2_1st_layer']), 
+        model.add(Dense(output_dim=params['units2'], 
+                        W_regularizer=l1l2(l1=params['l1_1st_layer'],l2=params['l2_1st_layer']), 
                         init = "glorot_uniform")) 
         # model.add(BatchNormalization())
-        model.add(Activation(self.params['activation']))
-        model.add(Dropout(self.params['dropout2']))
+        model.add(Activation(params['activation']))
+        model.add(Dropout(params['dropout2']))
         
-        if self.params['choice']['layers'] == 'three':
-            model.add(Dense(output_dim=self.params['choice']['units3'], 
-                            W_regularizer=l1l2(l1=self.params['l1_1st_layer'],l2=self.params['l2_1st_layer']), 
-                            init = "glorot_uniform")) 
+        if params['choice']['layers'] == 'three':
+            model.add(Dense(output_dim=params['choice']['units3'], 
+                            W_regularizer=l1l2(l1=params['l1_1st_layer'],l2=params['l2_1st_layer']), 
+                            init = "glorot_uniform"))
             model.add(BatchNormalization())
-            model.add(Activation(self.params['activation']))
-            model.add(Dropout(self.params['choice']['dropout3']))    
+            model.add(Activation(params['activation']))
+            model.add(Dropout(params['choice']['dropout3']))    
         
         model.add(Dense(1))
         # model.add(BatchNormalization())
         model.add(Activation('sigmoid'))
-        model.compile(loss='binary_crossentropy', optimizer=self.params['optimizer'])
+        model.compile(loss='binary_crossentropy', optimizer=params['optimizer'])
         
         self.model = model
 
-    def fit(self):
+    def fit(self, params):
         # save the the best weights out of all epoch runs
+        self.constructNeuronNetwork(params)
         callback1 = ModelCheckpoint(filepath = "callback_prediction.hdf5", 
                                     verbose = 0, 
                                     save_best_only = True)
         self.model.fit(self.train_X, 
                        self.train_y,  
-                       batch_size = self.params['batch_size'], 
-                       nb_epoch = self.params['nb_epochs'], 
+                       batch_size = params['batch_size'], 
+                       nb_epoch = params['nb_epochs'], 
                        validation_data = (self.test_X, self.test_y), 
                        callbacks = [callback1],
                        show_accuracy = True,
@@ -131,7 +132,7 @@ class DeepNeuronNetwork(object):
         self.model.load_weights("callback_prediction.hdf5")
         
         # latest version of keras requires re-compilation of model
-        self.model.compile(loss='binary_crossentropy', optimizer=self.params['optimizer'])  
+        self.model.compile(loss='binary_crossentropy', optimizer=params['optimizer'])  
         
         training_loss = self.eval(self.train_X, self.train_y)
         print('loss on the training set: %f.' % training_loss)
@@ -149,7 +150,7 @@ class DeepNeuronNetwork(object):
                                    sample_weight=None)
         return loss
         
-    def predict(self, X, model):        
+    def predict(self, X):        
         predRes = self.model.predict(X, batch_size = X.shape[0], verbose = 0) 
         return predRes[:,0]
     
@@ -169,10 +170,9 @@ class DeepNeuronNetwork(object):
             self.train_X, self.test_X = self.inputData[train_idx], self.inputData[test_idx] 
             self.train_y, self.test_y = self.outputData[train_idx], self.outputData[test_idx]
             
-            paramSpace = self.setParams()
-            self.constructNeuronNetwork()
+            self.setParams()
             trials = Trials()
-            optimalParam = fmin(self.fit, paramSpace, algo=tpe.suggest, max_evals=50, trials=trials) 
+            optimalParam = fmin(self.fit, self.params, algo=tpe.suggest, max_evals = self.maxEvals, trials=trials) 
             
             # stores the min loss and the best model of each CV run
             # ref: https://districtdatalabs.silvrback.com/parameter-tuning-with-hyperopt
@@ -190,14 +190,63 @@ class DeepNeuronNetwork(object):
         print('The min loss list for cross validation: \n%s\n' % lossRes)
         self.model = self.optimalModel[idx]
         print('The json formate best parameters: \n%s\n' % self.model.to_json())
-        
-if __name__ == '__main__':
+
+def getSearchPattern(loc, name):
+    '''
+    loc the location of file, containing only one column for indicated drug list
+    name a string specifies the name of the column
+    return: a string concatenated by '|'
+    '''
+    drugList = pd.read_csv(loc, header=None)
+    drugList.columns = [name]
+    drugList = [d.lower() for d in drugList[name]]
+    sePattern = "|".join(drugList)
+    return sePattern
+
+def getIndication(sePattern, drugList):
+    ''' 
+    use re package to find whether elements in drugList matches sePattern
+    return: a list with 1 indicating the matching the pattern, otherwise not
+    '''
+    idx = map(lambda x: int(bool(re.search(sePattern, x, re.IGNORECASE))), drugList)
+    return idx
+    
+def main():
     os.chdir("/exeh/exe3/zhaok/GE_result")
-    pheno = pd.read_csv("/exeh/exe3/zhaok/data/Cmap_differential_expression_anxiety_depression.csv", header=None)
-    X_orig = pheno.iloc[:, 2:].values 
-    y_orig = pheno.iloc[:, 1].values
-    drug_name = pheno.iloc[:, 0].values
-    self.fileName = 'keras_deep_learning_prediction.out'    
-    ##after the repeated cross-validation
-    model_result = run_cross_validation(X_orig, y_orig, drug_name)
-    print ("The negative log loss for Deep Learning: %s, average log loss: %s" % (model_result, np.mean(model_result)))
+    fileName = 'genePerturbation/keras_deep_learning_prediction.out' 
+    
+    loc = '/exeh/exe3/zhaok/data/N05A.txt'
+    sePattern = getSearchPattern(loc, 'drugName')
+    
+    drugBank = pd.read_table("genePerturbation/consensi-drugbank.tsv", header=0)
+    drugVoc = pd.read_csv('/exeh/exe3/zhaok/data/drugbank vocabulary.csv', header=0).iloc[:,[0,2]]
+    
+    # merge two data frames
+    drugVoc.columns = ['DBID', 'drugName']
+    newDrugBank = pd.merge(drugVoc, drugBank, left_on = 'DBID', right_on = 'perturbagen', suffixes=('', ''))
+    newDrugBank = newDrugBank.iloc[:, 1:]
+    
+    # generate indication
+    drugList = newDrugBank.loc[:,'drugName']
+    indication = getIndication(sePattern, drugList)
+    
+    X_orig = newDrugBank.iloc[:, 2:].values
+    y_orig = np.asarray(indication)
+    
+    DNN = DeepNeuronNetwork(X_orig, y_orig, fileName)
+    DNN.runCrossValidation()
+    print ("The negative log loss for Deep Learning: %s, average log loss: %s" % (DNN.minLosses, np.mean(DNN.minLosses)))
+    
+    predictionData = {'knockdown': 'consensi-knockdown.tsv',
+                      'overexpression':'consensi-overexpression.tsv',
+                      'pert_id':'consensi-pert_id.tsv'}
+    
+    for pertType in predictionData.keys():
+        pert = pd.read_table(predictionData[pertType], header=0)
+        pertRes = DNN.predict(pert.iloc[:, 1:].values)
+        resDict = {"predRes": pd.Series(pertRes), 'pertId': pert.loc[:, 'perturbagen']}
+        resDataFrame = pd.DataFrame(resDict)
+        resDataFrame.to_csv('DNN-'+pertType+'Res.csv')
+
+if __name__ == '__main__':    
+    main()
