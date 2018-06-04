331f46514178a0cd2bb8d7603078a303236195ac
Kai <zhaokai09@gmail.com>
2018-06-04 16:47:54 +0800

    Update h5Reader.R
    extension

--- h5Reader.R
+++ h5Reader.R
@@ -1,3 +1,7 @@
+# ----------------------------------------------------------------------------
+# read Pandas dataframe from h5 using h5 R package
+# ----------------------------------------------------------------------------
+
 require(h5)
 
 getdfFromH5 <- function(i, object, f){
@@ -19,3 +23,28 @@ loadhdf5data <- function(loc, objectName) {
 }
 
 # pheno <- loadhdf5data(loc, objectName)
+
+# ---------------------------------------------------------------------------------
+# read Pandas dataframe from h5 using rhdf5
+# ---------------------------------------------------------------------------------
+require(rhdf5)
+
+loadDataFrameFromH5 <- function(loc){
+    h5.structure <- h5ls(loc)
+    # Find all data nodes, values are stored in *_values and corresponding column
+    # titles in *_items
+    data_nodes <- grep("_values", h5.structure$name)
+    name_nodes <- grep("_items", h5.structure$name)
+
+    data_paths = paste(h5.structure$group[data_nodes], h5.structure$name[data_nodes], sep = "/")
+    name_paths = paste(h5.structure$group[name_nodes], h5.structure$name[name_nodes], sep = "/")
+
+    columns = list()
+    for (idx in seq(data_paths)) {
+      data <- as.data.frame(t(h5read(loc, data_paths[idx])))
+      names <- t(h5read(loc, name_paths[idx]))
+      colnames(data) <- names
+      columns <- append(columns, data)
+    }
+    data.frame(columns)
+}
